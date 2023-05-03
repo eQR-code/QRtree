21fde960cbb4f232b46069fcda47578ad277b72f
matteorosani <matteo.rosani@gmail.com>
2023-05-03 10:25:28 +0200

    Bug fix 8 bit mode encoding of QR code
    Co-authored-by: mattiascamuzzi <mattiascamuzzi@users.noreply.github.com>

--- decode/eQRcodeToeQRbytecode/main.py
+++ decode/eQRcodeToeQRbytecode/main.py
@@ -1,19 +1,42 @@
 import pyzbar.pyzbar as pyzbar
-from PIL import Image
+import cv2
 import os
 
 def decode(file):
     # Load the QR code PNG image
-    with Image.open(file) as img:
-        qr_data = pyzbar.decode(img)[0].data
+    img = cv2.imread(file)      #Takes the QRCode
+    
+    mask = cv2.inRange(img, (0,0,0), (100,100,100))
+    threasholded = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)       #mask to better decode the QRCode
+    img = 255-threasholded
 
-    i = 0 
-    while qr_data[i] == 48: # Removing padding: 48 is the ASCII code of '0'
+    codes = pyzbar.decode(img, symbols=[pyzbar.ZBarSymbol.QRCODE])
+
+    if len(codes) == 1:
+        qr_data = codes[0].data
+        qr_data = "".join([ f"{byte:0>8b}" for byte in _format_bytes(qr_data) ])
+
+        i = 0 
+        while qr_data[i] == "0": # Removing padding: 48 is the ASCII code of '0'
+            i += 1
+        i += 1 # Padding removed
+
+        header_length = 1 + 4 + 1 + 4 + 4 # Continuation + security profile + url + dialect + version
+
+        # Write the binary data to a file
+        with open(f"{os.path.splitext(file)[0]}.bin", 'w') as f:
+            f.write(qr_data[i + header_length:])
+    else:
+        raise Exception("No QRCode detected")
+
+def _format_bytes(data_in):
+    i = 0
+    while i < len(data_in):
+        byte = data_in[i]
         i += 1
-    i += 1 # Padding removed
-
-    header_length = 1 + 4 + 1 + 4 + 4 # Continuation + security profile + url + dialect + version
-
-    # Write the binary data to a file
-    with open(f"{os.path.splitext(file)[0]}.bin", 'w') as f:
-        f.write(qr_data[i + header_length:].decode('utf-8'))
+        if byte >= 194:
+            next_byte = data_in[i]
+            i += 1
+            yield int((byte & 0b00011111) << 6 | (next_byte & 0b00111111))
+        else:
+            yield int(byte)
--- encode/eQRbytecodeToeQRcode/main.py
+++ encode/eQRbytecodeToeQRcode/main.py
@@ -1,9 +1,10 @@
 import qrcode
+from qrcode.util import QRData, MODE_8BIT_BYTE
 import os
 
 def encode(inputFileName, outputFileName): 
 
-    with open(f"{os.path.splitext(inputFileName)[0]}.bin", 'rb') as f:
+    with open(f"{os.path.splitext(inputFileName)[0]}.bin", 'r') as f:
 
         data = f.read()
 
@@ -14,13 +15,13 @@ def encode(inputFileName, outputFileName):
         version = "0001"
         padding = "0" * ((8 - (len(continuation) + len(security_profile) + len(url) + len(dialect) + len(version) + len(data) + 1)) % 8) + "1"
 
-        header_data = bytes(f"{padding}{continuation}{security_profile}{url}{dialect}{version}", 'utf-8')
+        data = f"{padding}{continuation}{security_profile}{url}{dialect}{version}{data}"
 
-        data = header_data + data
+        data = bytes([ int(data[i:i + 8], 2) for i in range(0, len(data), 8) ])
 
         qr = qrcode.QRCode(version=None, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=10, border=4)     #Options for the QRCode
-        
-        qr.add_data(data, optimize=0)       #Adds the data to the QRCode
+
+        qr.add_data(QRData(data, MODE_8BIT_BYTE, False))       #Adds the data to the QRCode
 
         qr.make(fit=True)       #Generates the QRCode making sure that the dimension is compliant with the size of the data
 
