2cf86b2084955cb1f4d5155aede7266beeccc332
Stefano Scanzio <64889599+zioskenz@users.noreply.github.com>
2024-03-08 11:04:11 +0100

    Update QRtree.py
    Added license

--- QRtree.py
+++ QRtree.py
@@ -1,3 +1,15 @@
+############################################################################################################################
+# QRtree Software
+# This software is released under the GNU General Public License v3.0.
+#
+# The QRtree software is an implementation of the QRtree dialect, that allows the embedding of decision trees in a QR code.
+# For more information, read the file README.md.
+#
+# Please, cite this software (even if you just use a part of it) as:
+# S. Scanzio, M. Rosani, and M. Scamuzzi, “QRtree software,” GitHub. [Online]. Available: https://github.com/eQR-code/QRtree
+############################################################################################################################
+
+
 import argparse
 import os
 import webbrowser
